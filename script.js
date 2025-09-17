a7d2a84cf985ab793c329b011f227f2b01d3ba59
farahhammad <s202257640@kfupm.edu.sa>
2025-09-17 11:44:43 +0300

    TODO 4 done

--- script.js
+++ script.js
@@ -77,6 +77,13 @@ Task:
 2) Find and display the current day of month, month (0–11), and year.
 //    (Hint: getDate(), getMonth(), getFullYear() )
 */
+const d= new Date();
+console.log(d.getDate());
+console.log(d.getMonth());
+console.log(d.getFullYear());
+
+
+
 
 // ============================================================
 // TODO-5: ARRAY + SPREAD — find MIN and MAX from 10 numbers
